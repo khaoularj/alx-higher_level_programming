940dfeba63e7e52fa686802476712a5dbb31fd77
khaoularj <khaoula.raja1@gmail.com>
2023-08-28 09:01:34 -0700

    divides 2 integers and prints the result

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for element in my_list[:x]:
+    for element in my_list:
         try:
             if count < x and type(element) is int:
                 print("{:d}".format(element), end="")
                 count += 1
-        except IndexError:
+        except TypeError:
             continue
     print()
     return count

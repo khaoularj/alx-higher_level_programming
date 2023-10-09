1083e7cfa7a8056dd031fa5f9c01ea121475e345
khaoularj <khaoula.raja1@gmail.com>
2023-10-09 09:52:25 -0700

    Write a class BaseGeometry with Public instance method: def area(self)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Define an empty class"""
+
+
+class BaseGeometry:
+    """this is the new class"""
+
+
+    def area(self):
+        msg = "area() is not implemented"
+        raise Exception(msg)

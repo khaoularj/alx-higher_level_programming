c014507a57c4c4bbb811bc354dbafa1a45013f28
khaoularj <khaoula.raja1@gmail.com>
2023-10-11 12:41:22 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""define a function that writes a string to a text file"""
+
+
 def write_file(filename="", text=""):
     """function that writes a string to a text file (UTF8)
     and returns the number of characters written

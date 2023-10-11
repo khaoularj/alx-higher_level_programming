5f75d1e4ba703954cac11173a85ad9c08b18ac59
khaoularj <khaoula.raja1@gmail.com>
2023-10-11 12:53:22 -0700

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""define a function that returns the dictionary description"""
+
+
 def class_to_json(obj):
     """fn that returns the dictionary description with simple data structure"""
     return obj.__dict__

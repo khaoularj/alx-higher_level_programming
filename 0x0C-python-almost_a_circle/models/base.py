aba864b37d648a06d881add8eb137800ac98bae5
khaoularj <khaoula.raja1@gmail.com>
2023-10-16 08:10:31 -0700

    correct the bug in def to_json_string(list_dictionaries) in base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,7 +25,7 @@ class Base:
             list_dictionaries: is a list of dictionaries
         Return: the JSON serialization of a list_dictionaries"""
         if len(list_dictionaries) == 0 or list_dictionaries is None:
-            return []
+            return "[]"
         encod_json = json.dumps(list_dictionaries)
         return encod_json
 

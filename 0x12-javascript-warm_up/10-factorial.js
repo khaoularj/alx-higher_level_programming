97fe544081865413e1f0447731abea691bd59c1c
khaoularj <khaoula.raja1@gmail.com>
2023-12-11 07:03:57 -0800

    script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(n) || n === 0 || n === 1) {
+    return (1);
+  } else if (n < 0) {
+    return (-1);
+  } else {
+    return (n * factorial(n - 1));
+  }
+}
+console.log(factorial(Number(process.argv[2])));

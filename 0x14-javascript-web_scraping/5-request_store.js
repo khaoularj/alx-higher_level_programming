1eee5c0b3c26c89f06408e9b427a4396e3e3d1bf
khaoularj <khaoula.raja1@gmail.com>
2024-01-30 05:43:20 -0800

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const PathFl = process.argv[3];
+
+request(url).pipe(fs.createWriteStream(PathFl));

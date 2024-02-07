f253ce7c09f56f7ac3f8265f49f80ff970bd9b9e
khaoularj <khaoula.raja1@gmail.com>
2024-02-07 13:10:09 -0800

    js_jquery

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,4 @@
+#script that adds the class red to the <header> element when the user clicks on the tag DIV#red_header
+$('DIV#red_header').click(function () {
+  $('HEADER').addClass('red');
+});
---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,4 @@
+#script that fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
+$.get('https://swapi.co/api/people/5/?format=json', function (data) {
+  $('DIV#character').text(data.name);
+});
